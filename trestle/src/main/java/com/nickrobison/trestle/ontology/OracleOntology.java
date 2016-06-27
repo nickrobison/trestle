76730416d9559c3833df0139255440a832e81739
Nick Robison <nick@nickrobison.com>
2016-06-27 02:30:33 -0700

    Now correctly does the OWL inferencing and builds the graph and model correctly. Also returns both OWLObjectProperties and OWLDataProperties.
    Only returns a single DataProperty right now, should be updated at some point.

--- trestle/src/main/java/com/nickrobison/trestle/ontology/OracleOntology.java
+++ trestle/src/main/java/com/nickrobison/trestle/ontology/OracleOntology.java
@@ -1,13 +1,12 @@
 package com.nickrobison.trestle.ontology;
 
 import com.clarkparsia.pellet.owlapiv3.PelletReasoner;
+import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.query.*;
 import com.hp.hpl.jena.rdf.model.*;
 import com.nickrobison.trestle.common.EPSGParser;
-import oracle.spatial.rdf.client.jena.GraphOracleSem;
-import oracle.spatial.rdf.client.jena.ModelOracleSem;
-import oracle.spatial.rdf.client.jena.Oracle;
-import oracle.spatial.rdf.client.jena.OracleUtils;
+import oracle.spatial.rdf.client.jena.*;
+import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.formats.OWLXMLDocumentFormat;
 import org.semanticweb.owlapi.formats.RDFXMLDocumentFormat;
 import org.semanticweb.owlapi.model.*;
@@ -31,17 +30,16 @@ public class OracleOntology implements ITrestleOntology {
     private final PelletReasoner reasoner;
     private final DefaultPrefixManager pm;
     private final Oracle oracle;
-//    private final IOntologyDatabase database;
+    private final OWLDataFactory df;
     private final Model model;
     private final GraphOracleSem graph;
-
-//    Directly access the Jena model
 
     OracleOntology(String name, OWLOntology ont, DefaultPrefixManager pm, PelletReasoner reasoner, String connectionString, String username, String password) {
         this.ontologyName = name;
         this.ontology = ont;
         this.pm = pm;
         this.reasoner = reasoner;
+        this.df = OWLManager.getOWLDataFactory();
 //        try {
 //            this.database = new OracleDatabase(connectionString, username, password, ontologyName);
 //        } catch (SQLException e) {
@@ -49,12 +47,16 @@ public class OracleOntology implements ITrestleOntology {
 //        }
 
 //        Other ontology stuff
+
+        final Attachment owlprime = Attachment.createInstance(
+                new String[]{}, "OWLPRIME",
+                InferenceMaintenanceMode.NO_UPDATE, QueryOptions.DEFAULT);
         this.oracle = new Oracle(connectionString, username, password);
         try {
-//            Model oracleModel = ModelOracleSem.createOracleSemModel(oracle, ontologyName);
-//            this.model = ModelFactory.createOntologyModel(PelletReasonerFactory.THE_SPEC, oracleModel);
-            this.model = ModelOracleSem.createOracleSemModel(oracle, ontologyName);
-            this.graph = (GraphOracleSem) this.model.getGraph();
+//            We need this so that it actually creates the model if it doesn't exist
+            ModelOracleSem.createOracleSemModel(oracle, ontologyName);
+            this.graph = new GraphOracleSem(oracle, ontologyName, owlprime);
+            this.model = new ModelOracleSem(graph);
         } catch (SQLException e) {
             throw new RuntimeException("Can't create oracle model", e);
         }
@@ -158,22 +160,46 @@ public class OracleOntology implements ITrestleOntology {
 //        singleResource.listProperties();
     }
 
+//    TODO(nrobison): Make this return an iterator load into the HashSet
     public Optional<Set<OWLLiteral>> getIndividualProperty(OWLNamedIndividual individual, OWLDataProperty property) {
-//        I think we need to grab the individual from the database, an put it into the model, before trying to get the properties
-        final Optional<OWLNamedIndividual> ontologyIndividual = getIndividual(individual);
-        if (ontologyIndividual.isPresent()) {
-            final Set<OWLLiteral> dataPropertyValues = reasoner.getDataPropertyValues(ontologyIndividual.get(), property);
-            if (dataPropertyValues.isEmpty()) {
-                return Optional.empty();
-            } else {
-                return Optional.of(dataPropertyValues);
-            }
 
-        } else {
+        final Statement modelProperty = model.getProperty(model.getResource(getFullIRI(individual.getIRI()).toString()),
+                model.getProperty(getFullIRI(property.getIRI()).toString()));
+        if (modelProperty == null) {
+            logger.error("Property {} doesn't exist on individual {}", property.getIRI(), individual.getIRI());
             return Optional.empty();
         }
 
+//        Build and return the OWLLiteral
+        Set<OWLLiteral> properties = new HashSet<>();
 
+        final OWLDatatype owlDatatype = df.getOWLDatatype(IRI.create(modelProperty.getLiteral().getDatatypeURI()));
+        if (owlDatatype.getIRI().toString().equals("nothing")) {
+            logger.error("Datatype {} doesn't exist", modelProperty.getLiteral().getDatatypeURI());
+            return Optional.empty();
+        }
+        final OWLLiteral parsedLiteral = df.getOWLLiteral(modelProperty.getLiteral().getLexicalForm(), owlDatatype);
+        properties.add(parsedLiteral);
+        return Optional.of(properties);
+    }
+
+    public Optional<Set<OWLObjectProperty>> getIndividualObjectProperty(OWLNamedIndividual individual, OWLObjectProperty property) {
+        final Resource modelResource = model.getResource(getFullIRI(individual).toString());
+        final Property modelProperty = model.getProperty(getFullIRI(property).toString());
+        final StmtIterator stmtIterator = modelResource.listProperties(modelProperty);
+
+        Set<OWLObjectProperty> properties = new HashSet<>();
+        while (stmtIterator.hasNext()) {
+            final Statement statement = stmtIterator.nextStatement();
+            final OWLObjectProperty owlObjectProperty = df.getOWLObjectProperty(IRI.create(statement.getObject().toString()));
+            properties.add(owlObjectProperty);
+        }
+        if (properties.isEmpty()) {
+            logger.error("Individual {} has no properties {}", individual.getIRI(), property.getIRI());
+            return Optional.empty();
+        }
+
+        return Optional.of(properties);
     }
 
     /**
@@ -198,20 +224,17 @@ public class OracleOntology implements ITrestleOntology {
     }
 
     public void initializeOntology() {
-        logger.info("Removing all nodes from Oracle ontology {}", ontologyName);
-        model.removeAll();
-//        try {
-//            OracleUtils.dropSemanticModel(oracle, ontologyName);
-//        } catch (SQLException e) {
-//            logger.error("Cannot drop ontology {}", ontologyName, e);
-//        }
-
-//        OracleDatabase oraDB = connectToDatabase();
-
-//        Setup bulk import mode
-//        database.enableBulkLoading();
+        logger.info("Dropping Oracle ontology {}", ontologyName);
+        if (!model.isEmpty()) {
+            model.removeAll();
+        }
 
         //        We need to read out the ontology into a bytestream and then read it back into the oracle format
+        try {
+            this.graph.dropApplicationTableIndex();
+        } catch (SQLException e) {
+            logger.error("Cannot drop application index", e);
+        }
         logger.debug("Writing out the ontology to byte array");
         final ByteArrayOutputStream out = new ByteArrayOutputStream();
         try {
@@ -223,35 +246,42 @@ public class OracleOntology implements ITrestleOntology {
 
         final ByteArrayInputStream is = new ByteArrayInputStream(out.toByteArray());
         logger.debug("Reading model from byte stream");
+        logger.info("Creating new model {}", ontologyName);
+
         model.read(is, null);
 
-//        Finish the loading
-        logger.info("Rebuilding graph and indexes for {}", this.ontologyName);
+//        Setup Inference engine
         try {
-            this.graph.analyze();
+            runInference();
         } catch (SQLException e) {
-            logger.error("Cannot analyze {}", this.ontologyName, e);
+            logger.error("Cannot setup inference engine on {}", ontologyName, e);
         }
+
+        logger.info("Rebuilding indexes for {}", this.ontologyName);
+
         try {
             this.graph.rebuildApplicationTableIndex();
         } catch (SQLException e) {
             logger.error("Cannot rebuild indexes for {}", this.ontologyName, e);
         }
+
     }
 
-    //    TODO(nrobison): Close connection?
-//    private OracleDatabase connectToDatabase() {
-//
-//        OracleDatabase oraDB;
-////        final OWLDataFactory df = OWLManager.getOWLDataFactory();
-//        try {
-//            oraDB = new OracleDatabase();
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Problem with Oracle", e);
-//        }
-//
-//        return oraDB;
-//    }
+    /**
+     * Run the inference engine and rebuild the indexes.
+     * The inference engine is only run manually, via this method.
+     *
+     * @throws SQLException
+     */
+    public void runInference() throws SQLException {
+
+//        this.graph = new GraphOracleSem(oracle, ontologyName, owlprime);
+
+        logger.info("Rebuilding graph and performing inference");
+        this.graph.analyze();
+        this.graph.performInference();
+        this.graph.rebuildApplicationTableIndex();
+    }
 
     /**
      * Converts a prefixed IRI to the full one for the reasoner
@@ -271,6 +301,20 @@ public class OracleOntology implements ITrestleOntology {
 
     public IRI getFullIRI(String prefix, String suffix) {
         return getFullIRI(IRI.create(prefix, suffix));
+    }
+
+    public IRI getFullIRI(OWLNamedObject owlNamedObject) {
+        return getFullIRI(owlNamedObject.getIRI());
+    }
+
+    /**
+     * Return the number of asserted triples in the ontology
+     *
+     * @return long - Number of triples in ontology
+     */
+    public long getTripleCount() {
+
+        return this.graph.getCount(Triple.ANY);
     }
 
     /**
@@ -302,7 +346,7 @@ public class OracleOntology implements ITrestleOntology {
         reasoner.dispose();
         model.close();
         if (drop) {
-            logger.debug("Dropping model: {}", this.ontologyName);
+            logger.info("Dropping model: {}", this.ontologyName);
             try {
                 OracleUtils.dropSemanticModel(oracle, this.ontologyName);
             } catch (SQLException e) {
