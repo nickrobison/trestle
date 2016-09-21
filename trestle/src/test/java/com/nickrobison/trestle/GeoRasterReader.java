e1a7f5969ab31e4a83c360201a6ba926201296c0
Nick Robison <nick@nickrobison.com>
2016-09-21 12:00:55 -0700

    Initial GeoRaster test commit.
    Former-commit-id: c7b5ebaaaeb2f45e0e6affda486e6ed29d0e1532

--- trestle/src/test/java/com/nickrobison/trestle/GeoRasterReader.java
+++ trestle/src/test/java/com/nickrobison/trestle/GeoRasterReader.java
@@ -1,6 +1,5 @@
 package com.nickrobison.trestle;
 
-import org.geotools.coverage.grid.GridCoordinates2D;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.GridGeometry2D;
 import org.geotools.coverage.grid.io.AbstractGridFormat;
@@ -15,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.awt.image.Raster;
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -26,7 +26,7 @@ public class GeoRasterReader {
 
     private static final Logger logger = LoggerFactory.getLogger(GeoRasterReader.class);
     private GridCoverage2D grid;
-    private Raster data;
+    private RenderedImage renderedImage;
     private GeoTiffReader geoTiffReader;
     private GridEnvelope dimensions;
     private GridCoordinates maxDimensions;
@@ -38,7 +38,7 @@ public class GeoRasterReader {
         ParameterValue<OverviewPolicy> policy = AbstractGridFormat.OVERVIEW_POLICY.createValue();
         policy.setValue(OverviewPolicy.IGNORE);
 
-        //this will basically read 4 tiles worth of data at once from the disk...
+        //this will basically read 4 tiles worth of renderedImage at once from the disk...
         ParameterValue<String> gridsize = AbstractGridFormat.SUGGESTED_TILE_SIZE.createValue();
 
         //Setting read type: use JAI ImageRead (true) or ImageReaders read methods (false)
@@ -51,18 +51,20 @@ public class GeoRasterReader {
         maxDimensions = dimensions.getHigh();
         coverage = geoTiffReader.read(new GeneralParameterValue[]{policy, gridsize, useJaiRead});
         geometry = coverage.getGridGeometry();
+        renderedImage = coverage.getRenderedImage();
         readRaster();
     }
 
     private void readRaster() {
-//        final Rectangle bounds = data.getBounds();
+//        final Rectangle bounds = renderedImage.getBounds();
         final int numXTiles = maxDimensions.getCoordinateValue(0) + 1;
         final int numYTiles = maxDimensions.getCoordinateValue(1) + 1;
         int xTile, yTile, minX, minY;
         for (xTile = 0; xTile < numXTiles; xTile++) {
             for (yTile = 0; yTile < numYTiles; yTile++) {
                 final double[] pixel = new double[1];
-                coverage.evaluate(new GridCoordinates2D(xTile, yTile), pixel);
+                final Raster tile = renderedImage.getTile(xTile, yTile);
+//                coverage.evaluate(new GridCoordinates2D(xTile, yTile), pixel);
                 if (pixel[0] > 0) {
                     logger.info("Pixel {},{} has value {}", xTile, yTile, pixel);
                 }
