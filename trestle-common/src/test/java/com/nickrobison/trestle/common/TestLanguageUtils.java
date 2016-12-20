9ec3b6cd8363fe22a8af22d8045b8fc07d2f44d6
Nick Robison <nick@nickrobison.com>
2016-12-20 14:17:45 -0800

    Fixed language validator tests and added an additional check to watch for underscores. Also, bundled them up into a grouped assertion.
    Former-commit-id: 496e79e8ba263050058d269fb465c91425796f61

--- trestle-common/src/test/java/com/nickrobison/trestle/common/TestLanguageUtils.java
+++ trestle-common/src/test/java/com/nickrobison/trestle/common/TestLanguageUtils.java
@@ -3,6 +3,7 @@ package com.nickrobison.trestle.common;
 import org.junit.jupiter.api.Test;
 
 import static com.nickrobison.trestle.common.LanguageUtils.checkLanguageCodeIsValid;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -13,9 +14,10 @@ public class TestLanguageUtils {
 
     @Test
     public void testCodeValidation() {
-        assertTrue(checkLanguageCodeIsValid("en"));
-        assertTrue(checkLanguageCodeIsValid("en_GB"));
-        assertFalse(checkLanguageCodeIsValid("nick"));
-        assertFalse(checkLanguageCodeIsValid("fr_123_br"));
+        assertAll(() -> assertTrue(checkLanguageCodeIsValid("en"), "English should be ok"),
+                () -> assertTrue(checkLanguageCodeIsValid("en-GB"), "English GB should be ok"),
+                () -> assertFalse(checkLanguageCodeIsValid("en_GB"), "English underscores should fail"),
+                () -> assertFalse(checkLanguageCodeIsValid("nick"), "Nick should fail"),
+                () -> assertFalse(checkLanguageCodeIsValid("fr_123_br"), "Random string should fail"));
     }
 }
