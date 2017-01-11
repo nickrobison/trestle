82c1551369cd953a5a9be3ad2c0f634c92c1016c
Nick Robison <nick@nickrobison.com>
2017-01-11 01:27:39 -0800

    Fixed some garbage merge conflicts, no idea what blew up, but apparently something broke. I think I fixed it, for now.
    Former-commit-id: b6410a97de9a7e59191b0c2d993e455cae9be894

--- trestle-ontology/src/test/java/com/nickrobison/trestle/ontology/GraphDBOntologyTest.java
+++ trestle-ontology/src/test/java/com/nickrobison/trestle/ontology/GraphDBOntologyTest.java
@@ -1,3 +1,7 @@
+package com.nickrobison.trestle.ontology;
+
+import com.nickrobison.trestle.exceptions.MissingOntologyEntity;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.vocab.OWL2Datatype;
@@ -10,6 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Created by nrobison on 1/10/17.
  */
+@Tag("integration")
+@Tag("GraphDB")
 public class GraphDBOntologyTest extends OntologyTest {
     @Override
     void setupOntology() throws OWLOntologyCreationException {
@@ -108,125 +114,6 @@ public class GraphDBOntologyTest extends OntologyTest {
         assertEquals(Long.toString(negativeLong), individualDataProperty.get().stream().findFirst().get().getLiteral(), "Wrong long value");
 //        Oracle can't tell the difference between an int and a small long, so it treats them as longs.
         assertEquals(OWL2Datatype.XSD_LONG, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
-
-        aLong = df.getOWLDataProperty(IRI.create("trestle:", "neg_big_long"));
-        owlLiteral = df.getOWLLiteral(Long.toString(negativeBigLong), OWL2Datatype.XSD_LONG);
-        owlClassAssertionAxiom = df.getOWLClassAssertionAxiom(owlCl, long_test);
-        ontology.createIndividual(owlClassAssertionAxiom);
-        ontology.writeIndividualDataProperty(df.getOWLDataPropertyAssertionAxiom(aLong, long_test, owlLiteral));
-        individualDataProperty = ontology.getIndividualDataProperty(long_test, aLong);
-        assertEquals(Long.toString(negativeBigLong), individualDataProperty.get().stream().findFirst().get().getLiteral(), "Wrong long value");
-        assertEquals(OWL2Datatype.XSD_LONG, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
-    }
-}
-import org.semanticweb.owlapi.model.*;
-import org.semanticweb.owlapi.vocab.OWL2Datatype;
-
-import java.util.Optional;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-/**
- * Created by nrobison on 1/10/17.
- */
-public class GraphDBOntologyTest extends OntologyTest {
-    @Override
-    void setupOntology() throws OWLOntologyCreationException {
-        ontology = new OntologyBuilder()
-                .fromInputStream(inputStream)
-                .withDBConnection("graphdb", "", "")
-                .name("trestle")
-                .build();
-        ontology.initializeOntology();
-    }
-
-    @Override
-    void shutdownOntology() {
-        ontology.close(true);
-    }
-
-    @Override
-    public void testByteParsing() throws MissingOntologyEntity {
-        int smallInt = 4321;
-        int bigInt = Integer.MAX_VALUE;
-        int negativeInt = Integer.MIN_VALUE;
-        long smallLong = 4321;
-        long negativeLong = -4321;
-        long negativeBigLong = Long.MIN_VALUE;
-        long bigLong = Long.MAX_VALUE;
-        Double bigFloat = 4321.43;
-
-        final OWLNamedIndividual long_test = df.getOWLNamedIndividual(IRI.create("trestle:", "long_test"));
-        OWLDataProperty aLong = df.getOWLDataProperty(IRI.create("trestle:", "long_small"));
-        OWLLiteral owlLiteral = df.getOWLLiteral(Long.toString(smallLong), OWL2Datatype.XSD_LONG);
-        final OWLClass owlCl = df.getOWLClass(IRI.create("trestle:", "test"));
-        OWLClassAssertionAxiom owlClassAssertionAxiom = df.getOWLClassAssertionAxiom(owlCl, long_test);
-        ontology.createIndividual(owlClassAssertionAxiom);
-        ontology.writeIndividualDataProperty(df.getOWLDataPropertyAssertionAxiom(aLong, long_test, owlLiteral));
-        Optional<Set<OWLLiteral>> individualDataProperty = ontology.getIndividualDataProperty(long_test, aLong);
-        assertEquals(OWL2Datatype.XSD_INTEGER, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
-
-//        Big long
-        aLong = df.getOWLDataProperty(IRI.create("trestle:", "long_big"));
-        owlLiteral = df.getOWLLiteral(Long.toString(bigLong), OWL2Datatype.XSD_LONG);
-        owlClassAssertionAxiom = df.getOWLClassAssertionAxiom(owlCl, long_test);
-        ontology.createIndividual(owlClassAssertionAxiom);
-        ontology.writeIndividualDataProperty(df.getOWLDataPropertyAssertionAxiom(aLong, long_test, owlLiteral));
-        individualDataProperty = ontology.getIndividualDataProperty(long_test, aLong);
-        assertEquals(Long.toString(bigLong), individualDataProperty.get().stream().findFirst().get().getLiteral(), "Wrong long value");
-        assertEquals(OWL2Datatype.XSD_LONG, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
-
-//        Int
-        aLong = df.getOWLDataProperty(IRI.create("trestle:", "int_small"));
-        owlLiteral = df.getOWLLiteral(Integer.toString(smallInt), OWL2Datatype.XSD_INTEGER);
-        owlClassAssertionAxiom = df.getOWLClassAssertionAxiom(owlCl, long_test);
-        ontology.createIndividual(owlClassAssertionAxiom);
-        ontology.writeIndividualDataProperty(df.getOWLDataPropertyAssertionAxiom(aLong, long_test, owlLiteral));
-        individualDataProperty = ontology.getIndividualDataProperty(long_test, aLong);
-        assertEquals(Integer.toString(smallInt), individualDataProperty.get().stream().findFirst().get().getLiteral(), "Wrong long value");
-        assertEquals(OWL2Datatype.XSD_INTEGER, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
-
-        //        Big Int
-        aLong = df.getOWLDataProperty(IRI.create("trestle:", "int_big"));
-        owlLiteral = df.getOWLLiteral(Integer.toString(bigInt), OWL2Datatype.XSD_INTEGER);
-        owlClassAssertionAxiom = df.getOWLClassAssertionAxiom(owlCl, long_test);
-        ontology.createIndividual(owlClassAssertionAxiom);
-        ontology.writeIndividualDataProperty(df.getOWLDataPropertyAssertionAxiom(aLong, long_test, owlLiteral));
-        individualDataProperty = ontology.getIndividualDataProperty(long_test, aLong);
-        assertEquals(Integer.toString(bigInt), individualDataProperty.get().stream().findFirst().get().getLiteral(), "Wrong long value");
-        assertEquals(OWL2Datatype.XSD_INTEGER, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
-
-        //        Negative Int
-        aLong = df.getOWLDataProperty(IRI.create("trestle:", "neg_int"));
-        owlLiteral = df.getOWLLiteral(Integer.toString(negativeInt), OWL2Datatype.XSD_INTEGER);
-        owlClassAssertionAxiom = df.getOWLClassAssertionAxiom(owlCl, long_test);
-        ontology.createIndividual(owlClassAssertionAxiom);
-        ontology.writeIndividualDataProperty(df.getOWLDataPropertyAssertionAxiom(aLong, long_test, owlLiteral));
-        individualDataProperty = ontology.getIndividualDataProperty(long_test, aLong);
-        assertEquals(Integer.toString(negativeInt), individualDataProperty.get().stream().findFirst().get().getLiteral(), "Wrong long value");
-        assertEquals(OWL2Datatype.XSD_INTEGER, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
-
-        //        Double
-        aLong = df.getOWLDataProperty(IRI.create("trestle:", "float"));
-        owlLiteral = df.getOWLLiteral(Double.toString(bigFloat), OWL2Datatype.XSD_DECIMAL);
-        owlClassAssertionAxiom = df.getOWLClassAssertionAxiom(owlCl, long_test);
-        ontology.createIndividual(owlClassAssertionAxiom);
-        ontology.writeIndividualDataProperty(df.getOWLDataPropertyAssertionAxiom(aLong, long_test, owlLiteral));
-        individualDataProperty = ontology.getIndividualDataProperty(long_test, aLong);
-        assertEquals(Double.toString(bigFloat), individualDataProperty.get().stream().findFirst().get().getLiteral(), "Wrong long value");
-        assertEquals(OWL2Datatype.XSD_DECIMAL, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
-
-        //        Negative Long
-        aLong = df.getOWLDataProperty(IRI.create("trestle:", "neg_long"));
-        owlLiteral = df.getOWLLiteral(Long.toString(negativeLong), OWL2Datatype.XSD_LONG);
-        owlClassAssertionAxiom = df.getOWLClassAssertionAxiom(owlCl, long_test);
-        ontology.createIndividual(owlClassAssertionAxiom);
-        ontology.writeIndividualDataProperty(df.getOWLDataPropertyAssertionAxiom(aLong, long_test, owlLiteral));
-        individualDataProperty = ontology.getIndividualDataProperty(long_test, aLong);
-        assertEquals(Long.toString(negativeLong), individualDataProperty.get().stream().findFirst().get().getLiteral(), "Wrong long value");
-//        Oracle can't tell the difference between an int and a small long, so it treats them as longs.
-        assertEquals(OWL2Datatype.XSD_INTEGER, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
 
         aLong = df.getOWLDataProperty(IRI.create("trestle:", "neg_big_long"));
         owlLiteral = df.getOWLLiteral(Long.toString(negativeBigLong), OWL2Datatype.XSD_LONG);
