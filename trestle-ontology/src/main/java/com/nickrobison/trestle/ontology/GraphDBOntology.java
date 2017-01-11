17de1041f7db139222a44e1892432d19cd076b62
Nick Robison <nick@nickrobison.com>
2017-01-11 01:20:29 -0800

    TRESTLE-46: GraphDB compiles and runs, but it doesn’t actually work. No idea why.
    Former-commit-id: 1c07573c39c0311be147ccbbe933e59946d6f5b4

--- trestle-ontology/src/main/java/com/nickrobison/trestle/ontology/GraphDBOntology.java
+++ trestle-ontology/src/main/java/com/nickrobison/trestle/ontology/GraphDBOntology.java
@@ -1,6 +1,7 @@
 package com.nickrobison.trestle.ontology;
 
-import com.nickrobison.trestle.ontology.triplestore.GraphDBTripleStore;
+import afu.org.apache.commons.io.FileUtils;
+import com.nickrobison.trestle.ontology.types.TrestleResultSet;
 import com.ontotext.jena.SesameDataset;
 import com.ontotext.trree.OwlimSchemaRepository;
 import org.apache.jena.query.*;
@@ -9,6 +10,7 @@ import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.shared.Lock;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.repository.sail.SailRepositoryConnection;
+import org.eclipse.rdf4j.rio.RDFFormat;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.semanticweb.owlapi.util.DefaultPrefixManager;
@@ -16,97 +18,100 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by nrobison on 1/10/17.
  */
-@SuppressWarnings("Duplicates")
+
 public class GraphDBOntology extends JenaOntology {
 
     private static final Logger logger = LoggerFactory.getLogger(GraphDBOntology.class);
+    public static final String DATA_DIRECTORY = "./target/data";
     private static SailRepositoryConnection connection;
     private static SesameDataset dataset;
 
+
     GraphDBOntology(String ontologyName, OWLOntology ont, DefaultPrefixManager pm) {
-        super(ontologyName, GraphDBTripleStore.initializeGraphDBModel(), ont, pm);
-        connection = GraphDBTripleStore.getConnection();
-        dataset = GraphDBTripleStore.getDataset();
+        super(ontologyName, constructJenaModel(), ont, pm);
     }
 
-//    private static Model initializeGraphDBModel() {
-//        OwlimSchemaRepository schema = new OwlimSchemaRepository();
-//        schema.setDataDir(new File("./target/data"));
-//
-//        Map<String, String> modelParameters = new HashMap<>();
-//        modelParameters.put("storage-folder", "./");
-//        modelParameters.put("repository-type", "file-repository");
-//        modelParameters.put("ruleset", "owl2-rl");
-//
-//        schema.setParameters(modelParameters);
-//
-//        final SailRepository repository = new SailRepository(schema);
-//        repository.initialize();
-//        connection = repository.getConnection();
-//        dataset = new SesameDataset(connection);
-//        return ModelFactory.createModelForGraph(dataset.getDefaultGraph());
-//    }
+    private static Model constructJenaModel() {
+        OwlimSchemaRepository schema = new OwlimSchemaRepository();
+        schema.setDataDir(new File(DATA_DIRECTORY));
+        Map<String, String> schemaParams = new HashMap<>();
+        schemaParams.put("storage-folder", "./");
+        schemaParams.put("repository-type", "file-repository");
+        schemaParams.put("ruleset", "rdfs");
+        schemaParams.put("base-URL", "http://nickrobison.com/dissertation/trestle.owl#");
+        schema.setParameters(schemaParams);
+        final SailRepository repository = new SailRepository(schema);
+        repository.initialize();
+        connection = repository.getConnection();
+        dataset = new SesameDataset(connection);
+
+        return ModelFactory.createModelForGraph(dataset.getDefaultGraph());
+    }
 
     @Override
     public boolean isConsistent() {
-        logger.warn("Model validation is not implemented for {}", this.ontologyName);
-        return true;
+        return false;
     }
 
     @Override
     public void initializeOntology() {
-        logger.info("Dropping model {}", this.ontologyName);
+        logger.info("Dropping GraphDB ontology {}", ontologyName);
         if (!model.isEmpty()) {
             model.removeAll();
         }
 
-        logger.info("Writing new ontology");
+//        We need to read out the ontology into a bytestream and then read it back into the oracle format
+        logger.info("Creating new model {}", ontologyName);
+        logger.debug("Writing out the ontology to byte array");
 
         try {
-            this.model.read(ontologytoIS(this.ontology), null);
-        } catch (OWLOntologyStorageException e) {
-            logger.error("Cannot read ontology", e);
-            throw new RuntimeException("Cannot read ontology", e);
+            connection.begin();
+            connection.add(ontologytoIS(this.ontology), "urn:base", RDFFormat.RDFXML);
+            connection.commit();
+        } catch (OWLOntologyStorageException | IOException e) {
+            logger.error("Cannot read ontology into model", e);
+            throw new RuntimeException("Cannot read ontology in model", e);
         }
-        logger.debug("Finished writing ontology");
     }
 
     @Override
     public void close(boolean drop) {
         if (drop) {
-            logger.info("Dropping model {}", this.ontologyName);
-            this.model.removeAll();
+            this.model.close();
+            dataset.close();
+            connection.close();
+            logger.info("Dropping model {} at {}", this.ontologyName, DATA_DIRECTORY);
+            try {
+                FileUtils.deleteDirectory(new File(DATA_DIRECTORY));
+            } catch (IOException e) {
+                logger.error("Could not delete data directory {}", DATA_DIRECTORY, e);
+            }
         }
-        logger.info("Closing model", this.ontologyName);
-        this.model.close();
-        dataset.close();
-        connection.close();
         logger.debug("Opened {} transactions, committed {}", this.openedTransactions.get(), this.committedTransactions.get());
     }
 
     @Override
     public void runInference() {
-
     }
 
     @Override
-    public ResultSet executeSPARQL(String query) {
-        final Query queryString = QueryFactory.create(query);
-        ResultSet resultSet;
-        final QueryExecution queryExecution = QueryExecutionFactory.create(queryString, dataset);
+    public TrestleResultSet executeSPARQLTRS(String queryString) {
+        final TrestleResultSet resultSet;
+        final Query query = QueryFactory.create(queryString);
+        final QueryExecution qexec = QueryExecutionFactory.create(query, dataset);
         this.openTransaction(false);
         this.model.enterCriticalSection(Lock.READ);
         try {
-            resultSet = queryExecution.execSelect();
-            resultSet = ResultSetFactory.copyResults(resultSet);
+            resultSet = this.buildResultSet(qexec.execSelect());
         } finally {
-            queryExecution.close();
+            qexec.close();
             this.model.leaveCriticalSection();
             this.commitTransaction(false);
         }
@@ -115,22 +120,21 @@ public class GraphDBOntology extends JenaOntology {
 
     @Override
     public void openDatasetTransaction(boolean write) {
-        if (write) {
-            dataset.begin(ReadWrite.WRITE);
-            logger.debug("GraphDB write transaction opened");
-        } else {
-            dataset.begin(ReadWrite.READ);
-            logger.debug("GraphDB read transaction opened");
-        }
+        connection.begin();
+//        if (write) {
+//            connection.
+//            dataset.begin(ReadWrite.WRITE);
+//            logger.debug("GraphDB model write transaction opened");
+//        } else {
+//            dataset.begin(ReadWrite.READ);
+//            logger.debug("GraphDB model read transaction opened");
+//        }
     }
 
     @Override
     public void commitDatasetTransaction(boolean write) {
-        dataset.commit();
-        if (write) {
-            logger.debug("GraphDB write transaction committed");
-        } else {
-            logger.debug("GraphDB read transaction committed");
-        }
+        connection.commit();
+//        dataset.commit();
+        logger.debug("GraphDB model transaction committed");
     }
 }
--- trestle-ontology/src/test/java/com/nickrobison/trestle/ontology/GraphDBOntologyTest.java
+++ trestle-ontology/src/test/java/com/nickrobison/trestle/ontology/GraphDBOntologyTest.java
@@ -1,6 +1,7 @@
 package com.nickrobison.trestle.ontology;
 
 import com.nickrobison.trestle.exceptions.MissingOntologyEntity;
+import org.junit.jupiter.api.Test;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.vocab.OWL2Datatype;
 
@@ -29,6 +30,7 @@ public class GraphDBOntologyTest extends OntologyTest {
     }
 
     @Override
+    @Test
     public void testByteParsing() throws MissingOntologyEntity {
         int smallInt = 4321;
         int bigInt = Integer.MAX_VALUE;
@@ -47,7 +49,7 @@ public class GraphDBOntologyTest extends OntologyTest {
         ontology.createIndividual(owlClassAssertionAxiom);
         ontology.writeIndividualDataProperty(df.getOWLDataPropertyAssertionAxiom(aLong, long_test, owlLiteral));
         Optional<Set<OWLLiteral>> individualDataProperty = ontology.getIndividualDataProperty(long_test, aLong);
-        assertEquals(OWL2Datatype.XSD_INTEGER, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
+        assertEquals(OWL2Datatype.XSD_LONG, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
 
 //        Big long
         aLong = df.getOWLDataProperty(IRI.create("trestle:", "long_big"));
@@ -108,7 +110,7 @@ public class GraphDBOntologyTest extends OntologyTest {
         individualDataProperty = ontology.getIndividualDataProperty(long_test, aLong);
         assertEquals(Long.toString(negativeLong), individualDataProperty.get().stream().findFirst().get().getLiteral(), "Wrong long value");
 //        Oracle can't tell the difference between an int and a small long, so it treats them as longs.
-        assertEquals(OWL2Datatype.XSD_INTEGER, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
+        assertEquals(OWL2Datatype.XSD_LONG, individualDataProperty.get().stream().findFirst().get().getDatatype().getBuiltInDatatype(), "Should be long");
 
         aLong = df.getOWLDataProperty(IRI.create("trestle:", "neg_big_long"));
         owlLiteral = df.getOWLLiteral(Long.toString(negativeBigLong), OWL2Datatype.XSD_LONG);
