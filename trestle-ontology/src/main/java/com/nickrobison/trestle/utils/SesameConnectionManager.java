6515a7b35f66e83978f215b1b7e072237f9ef0b8
Nick Robison <nick@nickrobison.com>
2017-01-16 23:44:00 -0800

    TRESTLE-173: Initial commit of connection manager.
    Former-commit-id: 6ea7e887d1cb7b835ab5d565139fc116e644ea54

---
+++ trestle-ontology/src/main/java/com/nickrobison/trestle/utils/SesameConnectionManager.java
@@ -0,0 +1,88 @@
+package com.nickrobison.trestle.utils;
+
+import org.agrona.concurrent.ManyToManyConcurrentArrayQueue;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by nrobison on 1/16/17.
+ */
+public class SesameConnectionManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(SesameConnectionManager.class);
+    private final ManyToManyConcurrentArrayQueue<RepositoryConnection> connectionQueue;
+    private final Repository repository;
+
+
+    /**
+     * Create a new connection manager for a given {@link Repository}
+     * Initialize the pool with a specific size and create some initial {@link RepositoryConnection}
+     * @param repository - {@link Repository} to manage connections for
+     * @param poolSize - Size of connnection pool
+     * @param initialConnections - Initial connections to add to pool
+     */
+    public SesameConnectionManager(Repository repository, int poolSize, int initialConnections) {
+        this.connectionQueue = new ManyToManyConcurrentArrayQueue<>(poolSize);
+        this.repository = repository;
+
+        if (initialConnections < poolSize) {
+            throw new RuntimeException(String.format("Connection pool size is less than requested number of initial connections"));
+        }
+
+        logger.info("Initializing connection pool of size {} with {} initial connections", poolSize, initialConnections);
+        for (int i = 0; i < initialConnections; i++) {
+            returnConnection(createNewConnection());
+        }
+    }
+
+    /**
+     * Get a {@link RepositoryConnection} from the connection pool.
+     * If no connections are available, a new one is created
+     * @return - {@link RepositoryConnection}
+     */
+    public RepositoryConnection getConnection() {
+        final RepositoryConnection connection = this.connectionQueue.poll();
+        if (connection != null) {
+            logger.debug("Retrieving connection from pool {} connections still available", getConnectionCount());
+            return connection;
+        }
+        logger.debug("Connection pool exhausted, creating a new connection");
+        return createNewConnection();
+    }
+
+    /**
+     * Create  new {@link RepositoryConnection}
+     * @return - Newly created {@link RepositoryConnection}
+     */
+    private RepositoryConnection createNewConnection() {
+        logger.debug("Creating new connection to repository");
+        return this.repository.getConnection();
+    }
+
+    /**
+     * Returns connection to the pool
+     * If the pool is full, the connection is disposed of
+     * If the connection is still in a transaction, an error is thrown
+     * @param connection - {@link RepositoryConnection} to return to queue
+     */
+    private void returnConnection(RepositoryConnection connection) {
+        if (connection.isActive()) {
+            logger.error("Connection still has live transaction");
+        }
+        if (!this.connectionQueue.offer(connection)) {
+            logger.debug("Queue full, closing connection");
+            connection.close();
+        }
+        logger.debug("Returned connection to pool, {} available", getConnection());
+    }
+
+    /**
+     * Get the number of currently available connections
+     * @return - int of connections available in the pool
+     */
+    private int getConnectionCount() {
+        return this.connectionQueue.size();
+    }
+}
