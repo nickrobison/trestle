4b975fd14a853855e1d1b3721c92595943e84356
Nick Robison <nick@nickrobison.com>
2017-03-01 14:42:16 -0800

    EPICS-7: Removed AOT bootstrap file, because it’s garbage. maven now builds trestle-server correctly.
    Former-commit-id: fc31732c76df98bda938527aa9a7eb1aa75a4712

--- trestle-server/src/main/webapp/bootstrap.aot.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by nrobison on 2/3/17.
- */
-import {platformBrowser} from "@angular/platform-browser";
-import {AppModuleNgFactory} from "../../../aot/src/main/webapp/app/app.module.ngfactory";
-
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory).catch((err: Error) => console.error(err));
