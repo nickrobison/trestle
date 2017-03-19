87d96f1d3b89dc22fb55914e7ca623fbdf3f3c92
Nick Robison <nick@nickrobison.com>
2017-03-19 14:13:28 -0700

    TRESTLE-243: Fixed bad imports. Cleaned up tests to isolate static methods into their own test, so they only get initialized once. (+1 squashed commit) Squashed commits: [2ca3eb2] TRESTLE-243: Added gauge tests Removed ReflectASM dependency, since it doesn’t support static methods. Ugh.
    Former-commit-id: b1ed76e967aaf04be68c4f3ed709d6ca4656b991

--- trestle-metrics/src/test/java/com/nickrobison/trestle/metrics/MetricsCodeWeavingTest.java
+++ trestle-metrics/src/test/java/com/nickrobison/trestle/metrics/MetricsCodeWeavingTest.java
@@ -2,9 +2,11 @@ package com.nickrobison.trestle.metrics;
 
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.SharedMetricRegistries;
+import com.codahale.metrics.annotation.Gauge;
 import com.codahale.metrics.annotation.Metered;
 import com.nickrobison.trestle.annotations.metrics.Metriced;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -15,12 +17,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class MetricsCodeWeavingTest {
 
-    private static MetricRegistry registry;
+    private MetricRegistry registry;
 
-    @BeforeAll
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         registry = SharedMetricRegistries.getOrCreate("trestle-registry");
+    }
 
+    @AfterEach
+    public void reset() {
+        SharedMetricRegistries.clear();
     }
 
     @Test
@@ -31,16 +37,28 @@ public class MetricsCodeWeavingTest {
         testClass.testMeter();
         testClass.testMeter();
         assertEquals(2, registry.meter("com.nickrobison.trestle.metrics.MetricsCodeWeavingTest$TestClass.testMeter").getCount(), "Should be executed twice");
-//        Call static method
+
+    }
+
+    @Test
+    public void testTimer() {
+        assertEquals(0, registry.getGauges().size(), "Should have no gauges");
+        final TestClass testClass = new TestClass();
+        assertEquals(1, registry.getGauges().size(), "Should have a single gauge");
+        assertEquals(42, registry.gauge("com.nickrobison.trestle.metrics.MetricsCodeWeavingTest$TestClass.gauge-test", null).getValue(), "Gauge should read 1");
+
+
+    }
+
+    @Test
+    public void staticTest() {
+        //        Call static method
         TestStaticClass.testMeter();
         TestStaticClass.testMeter();
         TestStaticClass.testMeter();
         assertAll(() -> assertEquals(3, registry.meter("com.nickrobison.trestle.metrics.MetricsCodeWeavingTest$TestStaticClass.testMeter").getCount(), "Should have 3 static executions"),
-                () -> assertEquals(2, registry.meter("com.nickrobison.trestle.metrics.MetricsCodeWeavingTest$TestClass.testMeter").getCount(), "Should be executed twice"));
-
+                () -> assertEquals(7, registry.gauge("com.nickrobison.trestle.metrics.MetricsCodeWeavingTest$TestStaticClass.gauge-test-static", null).getValue(), "Should have 7 from static class"));;
     }
-
-
 
 
     @Metriced
@@ -50,6 +68,11 @@ public class MetricsCodeWeavingTest {
         @Metered
         public void testMeter() {
         }
+
+        @Gauge(name = "gauge-test")
+        int testGauge() {
+            return 42;
+        }
     }
 
     @Metriced
@@ -57,7 +80,11 @@ public class MetricsCodeWeavingTest {
 
         @Metered
         private static void testMeter() {
+        }
 
+        @Gauge(name = "gauge-test-static")
+        static int testGauge() {
+            return 7;
         }
     }
 }
