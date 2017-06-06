897a5b0c3ef73949b40dfe699f5d853b89bb67a9
Nick Robison <nick@nickrobison.com>
2017-06-06 13:59:09 -0700

    TRESTLE-343: Cleaned up login form and added error message to card footer.

--- trestle-server/src/main/webapp/app/NavigationModule/login/app.login.ts
+++ trestle-server/src/main/webapp/app/NavigationModule/login/app.login.ts
@@ -5,6 +5,7 @@ import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
 import {AuthService} from "../../UserModule/authentication.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {state, style, transition, trigger, animate} from "@angular/animations";
 
 interface IUserLogin {
     username: string;
@@ -14,14 +15,26 @@ interface IUserLogin {
 @Component({
     selector: "login",
     templateUrl: "./app.login.html",
-    styleUrls: ["./app.login.scss"]
+    styleUrls: ["./app.login.scss"],
+    animations: [
+        trigger("errorMessage", [
+            state("inactive", style({
+                backgroundColor: "white"
+            })),
+            state("active", style({
+                backgroundColor: "red"
+            })),
+            transition("inactive => active", animate("100ms ease-in")),
+            transition("active => inactive", animate("100ms ease-out"))
+        ])
+    ]
 })
 
 export class LoginComponent implements OnInit {
     loginForm: FormGroup;
-    public username: string;
-    public password: string;
-    returnUrl: string;
+    errorMessage: string;
+    errorState: string;
+    private returnUrl: string;
     constructor(private fb: FormBuilder, private authService: AuthService, private route: ActivatedRoute, private router: Router) {}
 
     ngOnInit(): void {
@@ -31,14 +44,20 @@ export class LoginComponent implements OnInit {
             "username": [null, Validators.required],
             "password": [null, Validators.required]
         });
+        this.errorMessage = "";
+        this.errorState = "inactive";
     }
 
     public login(user: IUserLogin) {
         console.debug("Logging in with", user.username, "and", user.password);
         this.authService.login(user.username, user.password).subscribe((data: any) => {
             this.router.navigate([this.returnUrl]);
-        }, (error: Error) => {
-            console.error("Error logging in", error);
+        }, (error: Response) => {
+           console.error("Error logging in: ", error.status);
+           if (error.status == 401) {
+               this.errorState = "active";
+               this.errorMessage = "Incorrect Username or Password";
+           }
         });
     }
 }
