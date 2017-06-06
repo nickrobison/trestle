32625234338be53831b06ffbacf532c906262102
Nick Robison <nick@nickrobison.com>
2017-06-06 13:30:17 -0700

    TRESTLE-342: Moved Login screen to reactive form.

--- trestle-server/src/main/webapp/app/NavigationModule/login/app.login.ts
+++ trestle-server/src/main/webapp/app/NavigationModule/login/app.login.ts
@@ -4,6 +4,12 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
 import {AuthService} from "../../UserModule/authentication.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+interface IUserLogin {
+    username: string;
+    password: string;
+}
 
 @Component({
     selector: "login",
@@ -12,20 +18,27 @@ import {AuthService} from "../../UserModule/authentication.service";
 })
 
 export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
     public username: string;
     public password: string;
     returnUrl: string;
-    constructor(private authService: AuthService, private route: ActivatedRoute, private router: Router) {}
+    constructor(private fb: FormBuilder, private authService: AuthService, private route: ActivatedRoute, private router: Router) {}
 
     ngOnInit(): void {
         this.authService.logout();
         this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
+        this.loginForm = this.fb.group({
+            "username": [null, Validators.required],
+            "password": [null, Validators.required]
+        });
     }
 
-    public login() {
-        console.debug("Logging in with", this.username, "and", this.password);
-        this.authService.login(this.username, this.password).subscribe((data: any) => {
+    public login(user: IUserLogin) {
+        console.debug("Logging in with", user.username, "and", user.password);
+        this.authService.login(user.username, user.password).subscribe((data: any) => {
             this.router.navigate([this.returnUrl]);
+        }, (error: Error) => {
+            console.error("Error logging in", error);
         });
     }
 }
