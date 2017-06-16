b8ecf2ce5364ccf42e7c3f4a133e109f650e3b32
Nick Robison <nick@nickrobison.com>
2017-06-16 16:19:43 -0700

    TRESTLE-352: Finalized javadoc for MergeEngine. #done

--- trestle-reasoner/src/main/java/com/nickrobison/trestle/reasoner/merge/TrestleMergeEngine.java
+++ trestle-reasoner/src/main/java/com/nickrobison/trestle/reasoner/merge/TrestleMergeEngine.java
@@ -12,20 +12,33 @@ import java.util.List;
 public interface TrestleMergeEngine {
     /**
      * Change to default {@link MergeStrategy}
+     *
      * @param strategy - new {@link MergeStrategy} to use for objects and facts
      */
     void changeDefaultMergeStrategy(MergeStrategy strategy);
 
+    /**
+     * Perform merge calculation using the default {@link MergeStrategy} set in the reasoner config
+     * Provides two {@link Temporal}s to determine where to set the valid/db merge points
+     * Throws a {@link TrestleMergeConflict} is the strategy is violated
+     *
+     * @param newFacts         - List of {@link OWLDataPropertyAssertionAxiom} to merge with existing facts
+     * @param existingFacts    - List of {@link TrestleResult} representing existing, currently valid facts
+     * @param eventTemporal    - {@link Temporal} to use as the new valid temporal
+     * @param databaseTemporal - {@link Temporal} to use as the new database temporal
+     */
     MergeScript mergeFacts(List<OWLDataPropertyAssertionAxiom> newFacts, List<TrestleResult> existingFacts, Temporal eventTemporal, Temporal databaseTemporal);
 
     /**
      * Perform merge calculation using provided {@link MergeStrategy}
+     * Provides two {@link Temporal}s to determine where to set the valid/db merge points
      * Throws a {@link TrestleMergeConflict} is the strategy is violated
-     * @param newFacts - List of {@link OWLDataPropertyAssertionAxiom} to merge with existing facts
-     * @param existingFacts - List of {@link TrestleResult} representing existing, currently valid facts
-     * @param eventTemporal
-     * @param databaseTemporal
-     * @param strategy - {@link MergeStrategy} to use when merging facts  @return - {@link MergeScript}
+     *
+     * @param newFacts         - List of {@link OWLDataPropertyAssertionAxiom} to merge with existing facts
+     * @param existingFacts    - List of {@link TrestleResult} representing existing, currently valid facts
+     * @param eventTemporal    - {@link Temporal} to use as the new valid temporal
+     * @param databaseTemporal - {@link Temporal} to use as the new database temporal
+     * @param strategy         - {@link MergeStrategy} to use when merging facts  @return - {@link MergeScript}
      */
     MergeScript mergeFacts(List<OWLDataPropertyAssertionAxiom> newFacts, List<TrestleResult> existingFacts, Temporal eventTemporal, Temporal databaseTemporal, MergeStrategy strategy);
 }
