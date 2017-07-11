888297de71017fa6997f133bbdd4a40476530849
Nick Robison <nick@nickrobison.com>
2017-07-11 16:11:55 -0700

    TRESTLE-313: Available datasets load correctly, also have a nice md-icon display. #time 1h 1m.

--- trestle-server/src/main/webapp/app/ExploreModule/viewer/viewer.component.ts
+++ trestle-server/src/main/webapp/app/ExploreModule/viewer/viewer.component.ts
@@ -1,10 +1,11 @@
 /**
  * Created by nrobison on 6/23/17.
  */
-import { ChangeDetectorRef, Component, NgZone, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { MapService } from "./map.service";
-import LngLatBounds = mapboxgl.LngLatBounds;
 import { ITrestleMapSource } from "../../UIModule/map/trestle-map.component";
+import LngLatBounds = mapboxgl.LngLatBounds;
+import { animate, state, style, transition, trigger } from "@angular/animations";
 
 enum DatasetState {
     UNLOADED,
@@ -12,40 +13,67 @@ enum DatasetState {
     LOADED
 }
 
+interface IDatasetState {
+    name: string,
+    state: DatasetState
+}
+
 @Component({
     selector: "dataset-viewer",
     templateUrl: "./viewer.component.html",
-    styleUrls: ["./viewer.component.css"]
+    styleUrls: ["./viewer.component.css"],
+    animations: [
+        trigger("fadeInOut", [
+            transition(":enter", [
+                style({transform: "scale(0)", opacity: 0}),
+                animate("500ms", style({transform: "scale(1)", opacity: 1}))
+            ]),
+            // transition(":leave", [
+            //     style({opacity: 1, transform: "scale(1)"}),
+            //     animate("200ms", style({opacity: 0, transform: "scale(0)"}))
+            // ])
+            // state("in", style({
+            //     opacity: "1.0"
+            // })),
+            // state("out", style({
+            //     opacity: "0.0"
+            // })),
+            // transition("in => out", animate("400ms ease-in-out")),
+            // transition("out => in", animate("400ms ease-in-out")),
+
+        ])
+    ]
 })
 export class DatsetViewerComponent implements OnInit {
-    // public availableDatasets: string[] = [];
+    public availableDatasets: IDatasetState[] = [];
     public DatasetState = DatasetState;
     public loadedDataset: ITrestleMapSource;
-    public datasetStates: Map<string, DatasetState> = new Map();
     private mapBounds: LngLatBounds;
 
-    constructor(private mapService: MapService, private ref: ChangeDetectorRef) {
+    constructor(private mapService: MapService) {
     }
 
     public ngOnInit(): void {
         this.mapService.getAvailableDatasets()
             .subscribe((results: string[]) => {
-                const map = new Map<string, DatasetState>();
                 results.forEach((ds) => {
-                    map.set(ds, DatasetState.UNLOADED);
+                    this.availableDatasets.push({
+                        name: ds,
+                        state: DatasetState.UNLOADED
+                    });
                 });
-                this.datasetStates = map;
-                console.debug("DS:", this.datasetStates);
-                // this.ref.detectChanges();
             });
     }
 
-    public loadDataset(dataset: string): void {
-        console.debug("Loading:", dataset);
-        this.datasetStates.set(dataset, DatasetState.LOADING);
-        this.mapService.stIntersect(dataset, this.mapBounds, new Date("1990-01-01"))
+    public loadDataset(dataset: IDatasetState): void {
+        if (dataset.state !== DatasetState.UNLOADED) {
+            return;
+        }
+        console.debug("Loading:", dataset.name);
+        dataset.state = DatasetState.LOADING;
+        this.mapService.stIntersect(dataset.name, this.mapBounds, new Date("1990-01-01"))
             .subscribe((data) => {
-                this.datasetStates.set(dataset, DatasetState.LOADED);
+                dataset.state = DatasetState.LOADED;
                 console.debug("Data:", data);
                 this.loadedDataset = {
                     id: "intersection-query",
