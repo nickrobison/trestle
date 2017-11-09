122991e5c46a4fd29c21ff52f87885649fab7156
Nick Robison <nick@nickrobison.com>
2017-11-08 16:31:41 -0800

    TRESTLE-523: I think this is already done. Seems like it's working, and enabled. #time 5m

--- trestle-reasoner/src/main/java/com/nickrobison/trestle/reasoner/caching/TrestleCache.java
+++ trestle-reasoner/src/main/java/com/nickrobison/trestle/reasoner/caching/TrestleCache.java
@@ -45,15 +45,6 @@ public interface TrestleCache {
      */
     void writeTrestleObject(TrestleIRI individualIRI, OffsetDateTime startTemporal, @Nullable OffsetDateTime endTemporal, OffsetDateTime dbStartTemporal, @Nullable OffsetDateTime dbEndTemporal, Object value);
 
-//    /**
-//     * Write TrestleObject
-//     *
-//     * @param individualIRI - {@link TrestleIRI} to add as key index/cache at a specific temporal interval
-//     * @param atTemporal - At temporal from Unix epoch (ms)
-//     * @param value - Value to write to the cache
-//     */
-//    void writeTrestleObject(TrestleIRI individualIRI, long atTemporal, @NonNull Object value);
-
     /**
      * Write TrestleObject to cache with a specified validity point, using {@link OffsetDateTime#now(ZoneId)} at {@link java.time.ZoneOffset#UTC} as the database temporal
      *
