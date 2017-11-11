18748d957f49660c49c0c058df6b6bbd162f3be3
Nick Robison <nick@nickrobison.com>
2017-11-10 16:57:24 -0800

    TRESTLE-363: Can now toggle the sidenav open and closed. Resize events are registered, but ViewChild is broken. #time 19m

--- trestle-server/src/main/webapp/app/app.component.ts
+++ trestle-server/src/main/webapp/app/app.component.ts
@@ -1,7 +1,7 @@
 /**
  * Created by nrobison on 1/19/17.
  */
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from "@angular/core";
+import { AfterViewInit, Component, HostListener, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from "@angular/core";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import { Subject } from "rxjs/Subject";
 import { Router } from "@angular/router";
@@ -9,6 +9,7 @@ import { AuthService, Privileges } from "./UserModule/authentication.service";
 import * as CryptoJS from "crypto-js";
 import { EventBus, UserLoginEvent } from "./UIModule/eventBus/eventBus.service";
 import { Subscription } from "rxjs/Subscription";
+import { MatSidenav } from "@angular/material";
 
 @Component({
     selector: "app-root",
@@ -17,14 +18,16 @@ import { Subscription } from "rxjs/Subscription";
     encapsulation: ViewEncapsulation.None
 })
 
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
     public userLoggedIn: Subject<boolean> = new BehaviorSubject<boolean>(false);
     public gravatarURL: string;
     // We need this in order to access the Privileges enum from the template
     public Privileges = Privileges;
+    // @ViewChild("sidenav") public something: MatSidenav;
     private loginSubscription: Subscription;
 
     constructor(private authService: AuthService, private router: Router, private eventBus: EventBus) {
+        console.debug("Width:", window.screen.width);
     }
 
     public ngOnInit(): void {
@@ -32,6 +35,13 @@ export class AppComponent implements OnInit, OnDestroy {
             console.debug("User event, is logged in?", event.isLoggedIn());
         });
         this.userLoggedIn.next(this.authService.loggedIn());
+    }
+
+    public ngAfterViewInit(): void {
+        if (window.screen.width < 400) {
+            console.debug("Small");
+            // this.something.close();
+        }
     }
 
     public ngOnDestroy(): void {
@@ -65,5 +75,9 @@ export class AppComponent implements OnInit, OnDestroy {
         this.userLoggedIn.next(false);
     }
 
-
+//    Resize function for sidenav
+    @HostListener("window:resize", ["$event"])
+    public onResize(event: any): void {
+        console.debug("resized", event.target.screen.width);
+    }
 }
