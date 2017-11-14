6ab702e43adc5732f51d8eaec894a0866fb3719b
Nick Robison <nick@nickrobison.com>
2017-11-14 12:11:57 -0800

    TRESTLE-541: Improved labeling. #time 7m

--- trestle-server/src/main/webapp/app/rxjs-operators.ts
+++ trestle-server/src/main/webapp/app/rxjs-operators.ts
@@ -5,6 +5,7 @@ import "rxjs/add/observable/forkJoin";
 import "rxjs/add/operator/debounceTime";
 import "rxjs/add/operator/distinctUntilChanged";
 import "rxjs/add/operator/filter";
+import "rxjs/add/operator/finally";
 import "rxjs/add/operator/first";
 import "rxjs/add/observable/from";
 import "rxjs/add/observable/fromEvent";
