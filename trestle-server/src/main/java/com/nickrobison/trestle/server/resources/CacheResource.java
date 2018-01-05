8393bd027586ce4fc1eefb9c7f2e9f11d5ec8911
Nick Robison <nick@nickrobison.com>
2018-01-04 16:01:52 -0800

    TRESTLE-570: Added Swagger documentation and improved handling of non-cache enabled instances. #time 10m

--- trestle-server/src/main/java/com/nickrobison/trestle/server/resources/CacheResource.java
+++ trestle-server/src/main/java/com/nickrobison/trestle/server/resources/CacheResource.java
@@ -5,6 +5,10 @@ import com.nickrobison.trestle.reasoner.caching.TrestleCacheStatistics;
 import com.nickrobison.trestle.server.annotations.AuthRequired;
 import com.nickrobison.trestle.server.auth.Privilege;
 import com.nickrobison.trestle.server.modules.ReasonerModule;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import javax.inject.Inject;
@@ -16,6 +20,7 @@ import javax.ws.rs.core.Response;
 @Path("/cache")
 @AuthRequired({Privilege.DBA})
 @Produces(MediaType.APPLICATION_JSON)
+@Api("cache")
 public class CacheResource {
     private final TrestleCache cache;
 
@@ -27,6 +32,12 @@ public class CacheResource {
 
     @GET
     @Path("/index")
+    @ApiOperation(value = "Get statistics for indexes",
+            notes = "Gets all relevant statistics for Trestle indexes",
+            response = TrestleCacheStatistics.class)
+    @ApiResponses({
+            @ApiResponse(code = 501, message = "Caching is disabled for this instance")
+    })
     public Response getIndexStatistics() {
         @Nullable final TrestleCacheStatistics cacheStatistics = this.cache.getCacheStatistics();
         if (cacheStatistics == null) {
@@ -39,6 +50,12 @@ public class CacheResource {
 
     @GET
     @Path("/rebuild/{index}")
+    @ApiOperation(value = "Rebuild index",
+            notes = "Rebuild the specified index. Which will requiring locking the indexes for all users")
+    @ApiResponses({
+            @ApiResponse(code = 500, message = "Unable to rebuild the index"),
+            @ApiResponse(code = 404, message = "Not a registered index")
+    })
     public Response rebuildIndex(@NotNull @PathParam(value = "index") String index) {
         switch (index) {
             case "valid": {
@@ -57,12 +74,18 @@ public class CacheResource {
                     return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e).build();
                 }
             }
-            default: return Response.status(Response.Status.NOT_IMPLEMENTED).entity("Not a recognized cache").build();
+            default:
+                return Response.status(Response.Status.NOT_FOUND).entity("Not a recognized cache").build();
         }
     }
 
     @GET
     @Path("/purge/{cache}")
+    @ApiOperation(value = "Purge cache",
+            notes = "Purge entries from the specified cache, and drop the relevant indexes")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Not a registered cache")
+    })
     public Response purgeCache(@PathParam("cache") String cacheToPurge) {
         if (cacheToPurge.equals("individual")) {
             this.cache.purgeIndividualCache();
@@ -71,6 +94,6 @@ public class CacheResource {
             this.cache.purgeObjectCache();
             return Response.ok().entity("Successfully purged Object cache").build();
         }
-        return Response.status(Response.Status.NOT_IMPLEMENTED).entity("Not a recognized cache").build();
+        return Response.status(Response.Status.NOT_FOUND).entity("Not a recognized cache").build();
     }
 }
