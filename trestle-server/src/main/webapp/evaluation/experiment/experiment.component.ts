b062e68293ee92edb2a85ee070469f6db5f76572
Nick Robison <nick@nickrobison.com>
2018-01-09 16:13:02 -0800

    TRESTLE-605: Individuals are on the map, it's gross, but it's there. #time 8m

--- trestle-server/src/main/webapp/evaluation/experiment/experiment.component.ts
+++ trestle-server/src/main/webapp/evaluation/experiment/experiment.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component } from "@angular/core";
 import { EvaluationService } from "../eval-service/evaluation.service";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import { MapSource } from "../../workspace/UIModule/map/trestle-map.component";
+import { TrestleTemporal } from "../../workspace/SharedModule/individual/TrestleIndividual/trestle-temporal";
 
 @Component({
     selector: "experiment",
@@ -14,10 +15,13 @@ export class ExperimentComponent implements AfterViewInit {
     public answered: string | undefined;
     public dataChanges: BehaviorSubject<MapSource | undefined>;
 
+    private maxHeight: number;
+
     public constructor(private es: EvaluationService) {
         this.experimentValue = 1;
 
         this.dataChanges = new BehaviorSubject(undefined);
+        this.maxHeight = 2016;
     }
 
     public ngAfterViewInit(): void {
@@ -35,6 +39,45 @@ export class ExperimentComponent implements AfterViewInit {
             .subscribe((experiment) => {
                 console.debug("has it:", experiment);
                 console.debug("Overlay?", this.es.isOverlay(experiment.state));
+                experiment.individuals.forEach((individual) => {
+
+                    const height = this.getHeight(individual.getTemporal());
+                    const baseHeight = ExperimentComponent.getBase(individual.getTemporal());
+                    this.dataChanges.next({
+                        id: individual.getID(),
+                        data: {
+                            type: "Feature",
+                            geometry: individual.getSpatialValue(),
+                            id: individual.getFilteredID(),
+                            properties: individual.getFactValues()
+                        },
+                        extrude: {
+                            id: individual.getID() + "-extrude",
+                            type: "fill-extrusion",
+                            source: individual.getID(),
+                            paint: {
+                                "fill-extrusion-color": "blue",
+                                "fill-extrusion-height": height,
+                                "fill-extrusion-base": baseHeight,
+                                "fill-extrusion-opacity": 0.7
+                            }
+                        },
+                        labelField: "adm2_name"
+                    });
+                });
             });
+    }
+
+    private getHeight(temporal: TrestleTemporal): number {
+        const to = temporal.getTo();
+        if (to === undefined) {
+            return this.maxHeight;
+        } else {
+            return to.get("year");
+        }
+    }
+
+    private static getBase(temporal: TrestleTemporal): number {
+        return temporal.getFrom().get("year");
     }
 }
