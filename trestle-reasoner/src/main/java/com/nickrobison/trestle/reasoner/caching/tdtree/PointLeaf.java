baf094dcbcad7308e95305d54061a0190df3cff2
Nick Robison <nick@nickrobison.com>
2018-01-29 11:33:41 -0800

    TRESTLE-612: Safevarargs to fix build. #time 2m

--- trestle-reasoner/src/main/java/com/nickrobison/trestle/reasoner/caching/tdtree/PointLeaf.java
+++ trestle-reasoner/src/main/java/com/nickrobison/trestle/reasoner/caching/tdtree/PointLeaf.java
@@ -30,7 +30,8 @@ public class PointLeaf<Value> extends LeafNode<Value> {
         logger.trace("Creating Point Node {}", this.getBinaryStringID());
     }
 
-    void copyInitialValues(@Nullable LeafKeySchema[] keys, @Nullable Value[] vals) {
+    @SafeVarargs
+    final void copyInitialValues(@Nullable LeafKeySchema[] keys, @Nullable Value... vals) {
         for (int i = 0; i < keys.length; i++) {
             if (keys[i] != null && vals[i] != null) {
                 this.values.put(keys[i], vals[i]);
