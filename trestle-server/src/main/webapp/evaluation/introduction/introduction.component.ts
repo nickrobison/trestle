79f3836bc333e1d850242789f4b546c464a94d91
Nick Robison <nick@nickrobison.com>
2018-02-02 16:58:46 -0800

    TRESTLE-605: Initial introduction flow and improved layout of pages. #time 1h

--- trestle-server/src/main/webapp/evaluation/introduction/introduction.component.ts
+++ trestle-server/src/main/webapp/evaluation/introduction/introduction.component.ts
@@ -1,12 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "introduction",
     templateUrl: "./introduction.component.html",
     styleUrls: ["./introduction.component.css"]
 })
-export class IntroductionComponent {
+export class IntroductionComponent implements OnInit {
 
-    public constructor() {
+    private gotoDemographics: boolean;
+    public introductionState: "intro" | "context" | "instructions";
+
+    public constructor(private router: Router) {
+        this.gotoDemographics = false;
+    }
+
+    public ngOnInit(): void {
+        this.introductionState = "intro";
+    }
+
+
+    public next(): void {
+        if (this.introductionState === "instructions") {
+            this.router.navigate(["/demographics"]);
+        } else if (this.introductionState === "intro") {
+            this.introductionState = "context";
+        } else if (this.introductionState === "context") {
+            this.introductionState = "instructions";
+        }
     }
 }
