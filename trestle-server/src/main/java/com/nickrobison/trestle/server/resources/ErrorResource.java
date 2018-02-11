99634251957181b85fd21645e91d101a3c2768d6
Nick Robison <nick@nickrobison.com>
2018-02-11 13:50:41 -0800

    TRESTLE-614: Added Swagger annotations to error resource

--- trestle-server/src/main/java/com/nickrobison/trestle/server/resources/ErrorResource.java
+++ trestle-server/src/main/java/com/nickrobison/trestle/server/resources/ErrorResource.java
@@ -3,6 +3,8 @@ package com.nickrobison.trestle.server.resources;
 import com.nickrobison.trestle.server.models.UIError;
 import com.nickrobison.trestle.server.models.UIErrorDAO;
 import io.dropwizard.hibernate.UnitOfWork;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +19,7 @@ import java.util.UUID;
  * Created by nickrobison on 2/5/18.
  */
 @Path("/error")
+@Api(value = "error")
 public class ErrorResource {
 
     private static final Logger logger = LoggerFactory.getLogger("UIErrorLogger");
@@ -31,9 +34,12 @@ public class ErrorResource {
     @POST
     @Path("/report")
     @UnitOfWork
+    @ApiOperation(value = "Reports a UI error to the server backend",
+    notes = "Adds an error, along with the associated statck trace to the backend logging system. Returns a UUID of the logged error",
+    response = UUID.class)
     public Response reportError(@Valid UIError error) throws Exception {
         final UUID errorID = this.errorDAO.create(error);
         logger.error("UI Error logged at {} with message: {}", error.getLocation(), error.getMessage());
         return Response.ok().entity(errorID).build();
     }
-}
+    }
