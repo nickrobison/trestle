9caa1cb9d801dbcd0bfac38ad45fac8cacd415f2
Nick Robison <nick@nickrobison.com>
2018-02-20 13:22:03 -0800

    TRESTLE-616: Stylesheets are now bundled into the vendor file. #time 25m

--- trestle-server/src/main/webapp/vendor.ts
+++ trestle-server/src/main/webapp/vendor.ts
@@ -1,22 +1,28 @@
 /**
  * Created by nrobison on 1/17/17.
  */
+// We need to require the css stylesheets as well
 import "@angular/core";
 import "@angular/http";
 import "@angular/common/http";
 import "@angular/forms";
 import "@angular/animations";
 import "@angular/material";
+import "@angular/material/_theming.scss";
 import "@mapbox/geojson-extent";
 import "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 import "hammerjs";
 import "materialize-css";
+import "materialize-css/dist/css/materialize.css";
 import "angular2-materialize";
 import "angular2-jwt";
 import "crypto-js";
 import "codemirror";
 import "codemirror/mode/javascript/javascript.js";
 import "codemirror/mode/sparql/sparql.js";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
 import "d3-array";
 import "d3-axis";
 import "d3-force";
