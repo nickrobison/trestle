62c42dca792d4b765b92c45a4a864d5742475b1b
Nick Robison <nick@nickrobison.com>
2018-04-17 15:22:27 -0700

    Refactor integration packages into trestle package.

---
+++ trestle-integrations/src/test/java/com/nickrobison/trestle/gaulintegrator/UnitTests/ReadShapes.java
@@ -0,0 +1,49 @@
+package com.nickrobison.trestle.gaulintegrator.UnitTests;
+
+import com.esri.shp.ShpHeader;
+import com.esri.shp.ShpReader;
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created by nrobison on 4/29/16.
+ */
+@Tag("tag")
+public class ReadShapes {
+    private static final Logger logger = Logger.getLogger(ReadShapes.class);
+    private final InputStream is = ReadShapes.class.getClassLoader().getResourceAsStream("shapefiles/combined_2000.shp");
+
+    @BeforeEach
+    public void setup() {
+        assertNotNull(is);
+    }
+
+    @Test
+    public void readShapefile() throws IOException {
+        final ShpReader shpReader = new ShpReader(new DataInputStream(is));
+        final ShpHeader shpHeader = shpReader.getHeader();
+        assertTrue(shpReader.hasMore());
+        int records = 0;
+        while (shpReader.hasMore()) {
+            shpReader.readPolygon();
+            records++;
+        }
+        assertEquals(1000, records);
+    }
+
+    @AfterEach
+    public void tearDown() throws IOException {
+        is.close();
+    }
+}
