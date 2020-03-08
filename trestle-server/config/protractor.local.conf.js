93698d11cdf7765c74f5a8b287ed3256341e1b41
Nick Robison <nicholas.a.robison@omb.eop.gov>
2020-03-08 13:07:33 -0400

    Server UI tests are mostly passing (one failure)
    Updated the protractor scripts to support testing locally.
    
    Updated the azure pipelines file to download the selenium container and skip building the javadocs.

--- trestle-server/config/protractor.local.conf.js
+++ trestle-server/config/protractor.local.conf.js
@@ -3,8 +3,8 @@
  */
 const helper = require("./helpers");
 exports.config = {
-    baseUrl: "http://vbox:8080/workspace/",
-    seleniumAddress: "http://vbox:4444/wd/hub",
+    baseUrl: "http://host.docker.internal:8080/workspace/",
+    seleniumAddress: "http://localhost:4444/wd/hub",
     // directConnect: true,
     capabilities: {
         "browserName": "chrome",
