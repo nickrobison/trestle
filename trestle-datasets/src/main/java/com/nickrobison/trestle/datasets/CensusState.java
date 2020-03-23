be7d2d2ccfb207afb5939d555ece97c9e5d57c12
Nick Robison <nickrobison@users.noreply.github.com>
2020-03-23 11:23:23 -0400

    Add simple support for CoVid data sources (#80)
    * Initial commit of CoVid data loading.
    
    * Update to latest Dropwizard and disable Swagger.
    
    The latest swagger package is causing issues with trying to load missing classes, so we'll just disable it for now.
    
    * Update node sass to latest version
    
    This allows us to run on Node 13, even though we're not using that for production or CI.
    
    * Trestle Server now supports our Covid repository
    
    We really need to come up with a way to support classes in a more flexible manner.
    This hardcoding is pretty gnarly.
    
    * Added parsing of Covid Case reports
    
    I think this should work to append various datasets to a single object, but we'll see.
    
    * Updated loader to add unassigned values to the states.
    
    Created a new CovidState class which can hold unassigned values.
    
    If one of our rows doesn't have a county fips code, then we just add it directly to the state itself.
    
    Co-authored-by: Nick Robison <nicholas.a.robison@omb.eop.gov>

---
+++ trestle-datasets/src/main/java/com/nickrobison/trestle/datasets/CensusState.java
@@ -0,0 +1,52 @@
+package com.nickrobison.trestle.datasets;
+
+import com.nickrobison.trestle.reasoner.annotations.DatasetClass;
+import com.nickrobison.trestle.reasoner.annotations.IndividualIdentifier;
+import com.nickrobison.trestle.reasoner.annotations.Spatial;
+import com.nickrobison.trestle.reasoner.annotations.temporal.StartTemporal;
+import org.locationtech.jts.geom.MultiPolygon;
+
+import java.time.LocalDate;
+
+/**
+ * Created by nickrobison on 3/22/20.
+ */
+@DatasetClass(name = "CensusState")
+public class CensusState {
+
+    private final MultiPolygon geom;
+    private final String geoid;
+    private final String name;
+    private final String abbreviation;
+    private final LocalDate start_date;
+
+    public CensusState(MultiPolygon geom, String geoid, String name, String abbreviation, LocalDate start_date) {
+        this.geom = geom;
+        this.geoid = geoid;
+        this.name = name;
+        this.abbreviation = abbreviation;
+        this.start_date = start_date;
+    }
+
+    @Spatial(projection = 4269)
+    public MultiPolygon getGeom() {
+        return geom;
+    }
+
+    @IndividualIdentifier
+    public String getGeoid() { return geoid; }
+
+
+    @StartTemporal(name = "start_date")
+    public LocalDate getStartDate() {
+        return start_date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+}
