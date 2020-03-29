4584fe705caedb95fa0daeac81caf8a4be9e3933
Nick Robison <nick@nickrobison.com>
2020-03-28 23:01:02 -0400

    TRESTLE-700: SpatialProjectionTest is now passing
    Added other shapefiles to the repo to get more of the spatial tests passing.

--- trestle-reasoner/src/test/java/com/nickrobison/trestle/reasoner/engines/spatial/SpatialProjectionTest.java
+++ trestle-reasoner/src/test/java/com/nickrobison/trestle/reasoner/engines/spatial/SpatialProjectionTest.java
@@ -25,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by nickrobison on 6/25/18.
  */
 @Tag("integration")
-@Disabled
+//@Disabled
 public class SpatialProjectionTest extends AbstractReasonerTest {
 
     @Test
