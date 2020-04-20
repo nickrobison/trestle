bfbf5d63c43c98c8ae3868d4b66ff5eed17b2688
Nick Robison <nick@nickrobison.com>
2020-04-20 11:52:10 -0400

    TRESTLE-728: Remove old Angular apps
    Remove the old workspace and evaluation apps.
    
    We did not port the eval app, just the workspace, so that one should be fully gone.

--- trestle-server/src/main/webapp/workspace/SharedModule/shared.config.ts
+++
@@ -1,6 +0,0 @@
-import { ICacheServiceConfig } from "./cache/cache.service.config";
-
-export const DATASET_CACHE_DI_CONFIG: ICacheServiceConfig = {
-    maxAge: 600000,
-    maxSize: 10
-};
--- trestle-server/src/main/webapp2/src/app/shared/cache/cache.service.config.ts
+++
@@ -1,7 +0,0 @@
-import { InjectionToken } from "@angular/core";
-
-export const CACHE_SERVICE_CONFIG = new InjectionToken<ICacheServiceConfig>("cache.service.config");
-export interface ICacheServiceConfig {
-    maxAge: number;
-    maxSize: number;
-}
