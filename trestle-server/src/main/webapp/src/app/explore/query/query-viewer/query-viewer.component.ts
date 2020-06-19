c792e75f705638c6be5ae143a7826b9a725d216d
Nick Robison <nick@nickrobison.com>
2020-06-19 15:03:44 -0400

    TRESTLE-739: Add broken pagination
    Try to do some pagination, it doesn't work, but it also doesn't hurt anything.
    
    We'll address this in a later ticket.

--- trestle-server/src/main/webapp/src/app/explore/query/query-viewer/query-viewer.component.ts
+++ trestle-server/src/main/webapp/src/app/explore/query/query-viewer/query-viewer.component.ts
@@ -1,23 +1,37 @@
 /**
  * Created by nrobison on 2/28/17.
  */
-import {Component, Input} from "@angular/core";
-import {ITrestleResultSet} from "../query.service";
+import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {ITrestleResultSet} from '../query.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
-    selector: "query-viewer",
-    templateUrl: "./query-viewer.component.html",
-    styleUrls: ["./query-viewer.component.scss"]
+  selector: 'query-viewer',
+  templateUrl: './query-viewer.component.html',
+  styleUrls: ['./query-viewer.component.scss']
 })
 
-export class QueryViewerComponent {
-    @Input("data") queryData: ITrestleResultSet;
+export class QueryViewerComponent implements OnInit, OnChanges {
 
-    constructor() {}
+  @Input('data')
+  public queryData: ITrestleResultSet;
+  @ViewChild(MatPaginator)
+  public paginator: MatPaginator;
+  public dataSource = new MatTableDataSource<ITrestleResultSet>([]);;
 
-    // ngOnChanges(changes: SimpleChanges): void {
-    //     if (!changes["queryData"].isFirstChange()) {
-    //         console.debug("Data changed to:", changes["queryData"].currentValue)
-    //     }
-    // }
+  constructor() {
+    // Not used
+  }
+
+  ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!changes['queryData'].isFirstChange()) {
+      console.debug('Data changed to:', changes['queryData'].currentValue);
+      this.dataSource.data = changes['queryData'].currentValue.results;
+    }
+  }
 }
