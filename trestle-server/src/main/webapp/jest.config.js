72cc9f17708a77ac01de71e4fb431af248d3345a
Nick Robison <nick@nickrobison.com>
2020-12-23 16:36:16 -0800

    Upgrade Jest to v26
    Upgrade to the latest version of Jest.
    
    This also upgrades the typings and angular integrations.
    
    Breaking change was that the tsConfig value is to the source root.

--- trestle-server/src/main/webapp/jest.config.js
+++ trestle-server/src/main/webapp/jest.config.js
@@ -2,13 +2,8 @@
 const jestPreset = require('jest-preset-angular/jest-preset');
 const { globals } = jestPreset;
 const tsjest = globals['ts-jest'];
-// set the correct path to the spect ts-config file
-// the default for the jest-preset-angular package
-// points to an incorrect path:
-// <rootDir/src/tsconfig.spec.js
 const tsjestOverrides = {
   ...tsjest
-  // tsConfig: '<rootDir>/tsconfig.spec.json'
 };
 const globalOverrides = {
   ...globals,
